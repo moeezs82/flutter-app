a892459f0de9172012549db34f912e0e3e6c9070
Muhammad Moeez <mora03033@gmail.com>
2022-08-07 17:30:05 +0500

    day 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:untitled3/pages/home_page.dart';
+import 'package:untitled3/pages/login_page.dart';
 
 void main(){
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
-  const myApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcomw to my App"),
-          ),
-        ),
+      // home: HomePage(),  //define in route
+      themeMode: ThemeMode.light,  //dark for dark mode
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple  //swatch to automatically adjust color
       ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      routes: {
+        "/": (context) => HomePage(),
+        "/login" : (context) => LoginPage()
+      },
     );
   }
 }
